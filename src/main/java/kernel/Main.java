a5f944e992640bf44d13068041e043381cccdc19
Joao Henrique <jharocha@gmail.com>
2020-03-12 22:50:16 -0300

    initial commit

---
+++ src/main/java/kernel/Main.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package kernel;
+
+/**
+ * @author joao
+ *
+ */
+public final class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		final String a = "a";
+		System.out.println(a);
+	}
+
+}
