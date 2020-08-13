fd605c387eea82aaf0d1a0f20868b53e487b4ec1
Camazaqui <jharocha@gmail.com>
2020-08-13 10:11:56 -0300

    inclui preCalculus interface

--- src/main/java/common/JclOptFactory.java
+++ src/main/java/common/JclOptFactory.java
@@ -1,15 +1,21 @@
 package common;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Properties;
+import java.util.TreeMap;
 
 import kernel.utils.FileHunter;
 
 public class JclOptFactory {
 	private static JclOptFactory instance;
+	private final Map<String, Object> singletons;
 
 	protected JclOptFactory() {
+		singletons = new TreeMap<>();
 	}
 
 	public static synchronized JclOptFactory instantiate() {
@@ -19,8 +25,33 @@ public class JclOptFactory {
 		return instance;
 	}
 
-	public Object getInstance(final String fileName)
-			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	public Object getInstance(final String nickname, final boolean isSingleton) {
+		if (nickname == null) {
+			return null;
+		}
+
+		if (isSingleton && singletons.containsKey(nickname)) {
+			return singletons.get(nickname);
+		}
+
+		final Object objInstance = returnInstance(nickname);
+
+		if (objInstance == null) {
+			return objInstance;
+		}
+
+		singletons.put(nickname, objInstance);
+		return objInstance;
+
+	}
+
+	private String relativise(final String filepath) {
+		final String base = System.getProperty("user.dir");
+
+		return new File(base).toURI().relativize(new File(filepath).toURI()).getPath();
+	}
+
+	public Object findInstance(final String fileName) {
 
 		Method m = null;
 		Object instance = null;
@@ -32,34 +63,24 @@ public class JclOptFactory {
 				final Class<?> c = extractClass(relativePath);
 				System.out.println(c);
 				m = c.getMethod("instantiate", null);
-				System.out.println(m);
 				instance = m.invoke(null, null);
 			}
-		} catch (final NoSuchMethodException | SecurityException | IllegalArgumentException e) {
+		} catch (final NoSuchMethodException | SecurityException | IllegalArgumentException | IllegalAccessException
+				| InvocationTargetException e) {
 			return null;
 		}
 
 		return instance;
 	}
 
-	private String relativise(final String filepath) {
-		final String base = System.getProperty("user.dir");
-
-		return new File(base).toURI().relativize(new File(filepath).toURI()).getPath();
-	}
-
 	@SuppressWarnings("finally")
 	private Class<?> extractClass(final String relativeFilepath) {
-
 		final String dotedString = relativeFilepath.replace('/', '.');
-		System.out.println(dotedString);
 		final String[] possiblePackage = dotedString.split("\\.");
-		System.out.println(possiblePackage);
 		Class<?> minhaClasse = null;
 		final StringBuilder sb = new StringBuilder();
 		for (int i = possiblePackage.length - 2; i > 0; i--) {
 			sb.insert(0, possiblePackage[i]);
-			System.out.println(sb.toString());
 			try {
 				minhaClasse = Class.forName(sb.toString());
 			} finally {
@@ -67,6 +88,23 @@ public class JclOptFactory {
 				continue;
 			}
 		}
+
 		return minhaClasse;
 	}
+
+	private Object returnInstance(final String nickname) {
+
+		final Properties properties = new Properties();
+		try {
+			properties.load(new FileInputStream("JCLopt_naming.properties"));
+			final Class<?> c = Class.forName(properties.getProperty(nickname));
+			return c.newInstance();
+		} catch (final Exception e) {
+			e.printStackTrace();
+		}
+
+		// otherwise
+		return null;
+	}
+
 }
